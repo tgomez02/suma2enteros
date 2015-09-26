f02eb1bf38e092e986c87a91ab60ce0e1e7cf537
tgomez02 <tgomez02@hotmail.com>
2015-09-26 17:17:33 +0200

    Update JavaApplication25.java

--- JavaApplication25/src/javaapplication25/JavaApplication25.java
+++ JavaApplication25/src/javaapplication25/JavaApplication25.java
@@ -15,6 +15,7 @@ public class JavaApplication25 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("se ha modificado el codigo");
         // TODO code application logic here
     }
     
